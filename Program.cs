8f65f471a69adb618ca01b290d8ffd3b6b87508e
Collin Hintzke <collinhintzke@outlook.com>
2021-08-13 14:35:46 -0700

    Hello World working

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
